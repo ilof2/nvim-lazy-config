54e18e7b318fd7dc5fd7e38e54187753e62a60f1
ilof2 <ilof@tuta.io>
2024-05-09 14:08:45 +0200

    harpoon added

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -19,7 +19,8 @@ map("n", "<C-l>", "<C-w>l", opts)
 map("n", "<leader>sv", "<C-w>v", opts)
 map("n", "<leader>sh", "<C-w>s", opts)
 map("n", "<leader>se", "<C-w>=", opts)
-map("n", "<leader>x", "<cmd>close<cr>", opts)
+map("n", "<leader>x", "<cmd>:enew|bd#<cr>", opts)
+map("n", "<leader>xa", "<cmd>:close<cr>", opts)
 
 map("n", "<leader>qq", ":qall!<CR>", opts)
 
@@ -34,25 +35,26 @@ map("v", "<S-j>", ":m '>+1<CR>gv=gv", opts)
 map("v", "<S-k>", ":m '<-2<CR>gv=gv", opts)
 
 -- Navigate buffers
-map("n", "<S-h>", ":bnext<CR>", opts)
-map("n", "<S-l>", ":bprevious<CR>", opts)
+-- map("n", "<S-h>", ":bnext<CR>", opts)
+-- map("n", "<S-l>", ":bprevious<CR>", opts)
 
 map("n", "<leader>fk", ":cprev<cr>", opts)
 map("n", "<leader>fj", ":cnext<cr>", opts)
 
 -- Save shorcuts
-map("n", "<leader>w", ":w!<CR>", opts)
+map("n", "<leader>w", ":wa! | echo 'All files saved'<CR>", opts)
 
 map("n", "<leader><tab>", ":NvimTreeToggle<cr>", opts)
 
 -- Telescope --
 map("n", "<leader>ff", "<cmd>Telescope find_files<cr>", opts)
 map("n", "<leader>fl", "<cmd>Telescope live_grep<cr>", opts)
-map("n", "<leader>fs", "<cmd>Telescope grep_string<cr>", opts)
+map("n", "<leader>fs", "<cmd>Telescope resume<cr>", opts)
 -- quick search something --
 map("v", "<leader>f", "y<ESC>:Telescope live_grep default_text=<c-r>0<CR>", opts)
 
 
 map("n", "<leader>pv", ":lua require('swenv.api').pick_venv()<cr>", opts)
 map("n", "<leader>lo", ":Lspsaga outline<cr>", opts)
+map("n", "<leader>\\", ":Lspsaga term_toggle<cr>", opts)
 map("n", "<leader>,", ":nohlsearch<CR>", opts)
--- lua/user/plugins/lualine.lua
+++ lua/user/plugins/lualine.lua
@@ -1,53 +1,53 @@
 return {
-"nvim-lualine/lualine.nvim",
-dependencies = {"nvim-tree/nvim-web-devicons"},
-lazy = false,
-config = function ()
-  local lualine = require("lualine")
-  lualine.setup {
-  options = {
-      icons_enabled = true,
-      theme = 'auto',
-      component_separators = { left = '', right = ''},
-      section_separators = { left = '', right = ''},
-      disabled_filetypes = {
-        statusline = {},
-        winbar = {},
-      },
-      ignore_focus = {},
-      always_divide_middle = true,
-      globalstatus = false,
-      refresh = {
-        statusline = 1000,
-        tabline = 1000,
-        winbar = 1000,
-      }
-    },
-    sections = {
-      lualine_a = {'mode', 'swenv'},
-      lualine_b = {'branch', 'diff', 'diagnostics'},
-      lualine_c = {'filename'},
-      lualine_x = {'encoding', 'filetype', {
-        require("noice").api.statusline.mode.get,
-        cond = require("noice").api.statusline.mode.has,
-        color = { fg = "#ff9e64" },
-      }},
-      lualine_y = {'progress'},
-      lualine_z = {'location'}
-    },
-    inactive_sections = {
-      lualine_a = {},
-      lualine_b = {},
-      lualine_c = {'filename'},
-      lualine_x = {'location'},
-      lualine_y = {},
-      lualine_z = {}
-    },
-    tabline = {},
-    winbar = {},
-    inactive_winbar = {},
-    extensions = {}
-  }
-end
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    lazy = false,
+    config = function()
+        local lualine = require("lualine")
+        lualine.setup {
+            options = {
+                icons_enabled = true,
+                theme = 'auto',
+                component_separators = { left = '', right = '' },
+                section_separators = { left = '', right = '' },
+                disabled_filetypes = {
+                    statusline = {},
+                    winbar = {},
+                },
+                ignore_focus = {},
+                always_divide_middle = true,
+                globalstatus = false,
+                refresh = {
+                    statusline = 1000,
+                    tabline = 1000,
+                    winbar = 1000,
+                }
+            },
+            sections = {
+                lualine_a = { 'mode', 'swenv' },
+                lualine_b = { 'branch', 'diff', 'diagnostics' },
+                lualine_c = { 'filename', 'harpoon2' },
+                lualine_x = { 'encoding', 'filetype', {
+                    require("noice").api.statusline.mode.get,
+                    cond = require("noice").api.statusline.mode.has,
+                    color = { fg = "#ff9e64" },
+                } },
+                lualine_y = { 'progress' },
+                lualine_z = { 'location' }
+            },
+            inactive_sections = {
+                lualine_a = {},
+                lualine_b = {},
+                lualine_c = { 'filename' },
+                lualine_x = { 'location' },
+                lualine_y = {},
+                lualine_z = {}
+            },
+            tabline = {},
+            winbar = {},
+            inactive_winbar = {},
+            extensions = {}
+        }
+    end
 
 }
